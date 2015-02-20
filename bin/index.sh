1d431d5f76b0aaf77d535008e21de2155662f2f5
Eric Moritz <eric@themoritzfamily.com>
2015-02-20 00:15:21 -0800

    added the #!

--- bin/index.sh
+++ bin/index.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 HERE=$(dirname $0)
 
 ${SPARK_HOME}/bin/spark-submit --master local[4] --jars $(ls ${HERE}/../target/universal/stage/lib/*.jar | tr "\n" ,) --class "mrs.app.MRSApp" ${HERE}/../target/scala-2.10/music-review-spider_2.10-latest.jar ${SPARQL_QUERY} ${SPARQL_UPDATE}
